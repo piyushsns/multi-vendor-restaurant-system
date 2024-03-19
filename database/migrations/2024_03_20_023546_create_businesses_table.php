0767b098e3c460c8a4ea4d8554cd2a434e958430
Piyush Jain SNS <piyush.jain@snssystem.com>
2024-03-20 02:08:42 +0530

    added chnage sin mail config

---
+++ database/migrations/2024_03_20_023546_create_businesses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('business_name')->nullable();
+            $table->string('website')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('address')->nullable();
+            $table->string('fax')->nullable();
+            $table->timestamps();           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+}
